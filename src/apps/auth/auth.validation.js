5025927bf638351fa66b2832331a09d721e26121
zumerhub <zumerhub@gmail.com>
2024-04-25 23:37:23 +0100

    update index

--- src/apps/auth/auth.validation.js
+++ src/apps/auth/auth.validation.js
@@ -1,15 +1,5 @@
 const Joi = require('joi');
 
-// function updateForgothValidation(data) {
-//     const schema = Joi.object({
-//         name: Joi.string().min(5).max(255).required(),
-//         email: Joi.string().email().required(),
-//         password: Joi.string().min(8).required()
-//     });
-
-//     return schema.validate(data);
-// }
-
 function loginValidation(data) {
     const schema = Joi.object({
         email: Joi.string().email().required(),
--- src/index.js
+++ src/index.js
@@ -25,41 +25,6 @@ res.send('Running server!!!!!!!!')
 })
 
 
-
-// // Create a Nodemailer transporter
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.ethereal.email',
-//   port: 587,
-//   secure: true, // Use `true` for port 465, `false` for all other ports
-//   auth: {
-//     user: 'testing1@test.com',
-//     pass: 'jn7jnAPss4f63QBp6D',
-//   },
-// });
-
-// // Define an endpoint to send emails
-// app.post('/send-email', async (req, res) => {
-//   try {
-//     const { to, subject, text, html } = req.body;
-
-//     // Send mail with defined transport object
-//     const info = await transporter.sendMail({
-//       from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
-//       to: to, // List of receivers
-//       subject: subject,
-//       text: text,
-//       html: html,
-//     });
-
-//     console.log('Message sent: %s', info.messageId);
-//     res.status(200).json({ message: 'Email sent successfully', messageId: info.messageId });
-//   } catch (error) {
-//     console.error('Error sending email:', error);
-//     res.status(500).json({ error: 'Failed to send email' });
-//   }
-// });
-
-
 const Port = process.env.Port || 5000 ;
 
 app.listen(Port, () => console.log(`Server Running on Port ${Port}...`));
